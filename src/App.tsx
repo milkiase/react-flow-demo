fd9cb210d9a67e276fd02de70f301ef35b4a5945
milkiase <milktes27@gmail.com>
2023-10-08 08:30:53 +0300

    implemented the copy, cut and paste functionality for the flow elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useCallback, useRef, MouseEvent, TouchEvent} from 'react';
-import ReactFlow, {Node, Panel, Controls, MiniMap, OnConnectStart, OnConnectEnd,
-  ConnectionLineType, SelectionMode, useStoreApi, OnConnectStartParams
+import { useCallback, useRef, useState, useMemo, MouseEvent, TouchEvent, memo} from 'react';
+import ReactFlow, {Node, Edge, Panel, Controls, MiniMap, OnConnectStart, OnConnectEnd, useReactFlow,
+  ConnectionLineType, SelectionMode, useStoreApi, OnConnectStartParams, OnSelectionChangeFunc, OnSelectionChangeParams
 } from 'reactflow';
 import { shallow } from 'zustand/shallow';
 import useStore, {RFState} from './store/store';
@@ -11,6 +11,7 @@ import DeleteBtnEdge from './customEdges/DeleteBtnEdge/DeleteBtnEdge.component';
 
 import 'reactflow/dist/style.css'
 import Modal from './components/Modal.component';
+import { nanoid } from 'nanoid/non-secure';
 
 const nodeTypes = {
   textUpdater: TextUpdaterNode,
@@ -42,12 +43,17 @@ const defultEdgeOptions = {
 }
 
 
-function App() {
+const App = memo(()=>{
+  const reactFlowInstance = useReactFlow()
   const {
     nodes, edges, onNodesChange, onEdgesChange,
     onConnect, addNode, showModal, setShowModal, setModalInfo
   } = useStore(selector, shallow);
-  
+
+  const [selection, setSelection] = useState({nodes: [], edges: []} as OnSelectionChangeParams)
+  const [copiedSelection, setCopiedSelection] = useState<OnSelectionChangeParams | null>(null)
+
+  const canCopy = useMemo(():boolean=> selection.nodes.length > 0, [selection])
   const store = useStoreApi()
   const connectStartRef = useRef<ConnectStartRef>(null)
   const nodeColor = (node: Node):string=>{
@@ -75,8 +81,8 @@ function App() {
       const sourceEventPosition = {clientX: (connectStartRef.current?.event as MouseEvent)?.clientX,
         clientY: (connectStartRef.current?.event as MouseEvent)?.clientY,
       }
-      const differenceY = (sourceEventPosition.clientY as number) - (event as unknown as MouseEvent).clientY
-      const differenceX = (sourceEventPosition.clientX as number) - (event as unknown as MouseEvent).clientX
+      const differenceY = ((sourceEventPosition.clientY as number) - (event as unknown as MouseEvent).clientY)
+      const differenceX = ((sourceEventPosition.clientX as number) - (event as unknown as MouseEvent).clientX)
 
       addNode(sourceNode, {x: sourceNode.position.x - differenceX, y: sourceNode.position.y - differenceY})
       connectStartRef.current = null
@@ -93,6 +99,62 @@ function App() {
       setShowModal(true)
     }
   }, [])
+
+  const onSelectionChange:OnSelectionChangeFunc = useCallback((params)=>{
+    setSelection(params)
+  }, [])
+
+  const cutHandler = useCallback(()=>{
+    setCopiedSelection(selection)
+    reactFlowInstance.deleteElements(selection)
+  }, [reactFlowInstance, selection])
+
+  const copyHandler = useCallback(()=>{
+    setCopiedSelection(selection)
+  }, [selection])
+
+  const pasteHandler = useCallback(()=>{
+    // replacing duplicate node's id by new id and and updating it in the copiedEdges if it is used
+    // as a source or target
+    (copiedSelection as OnSelectionChangeParams).nodes = copiedSelection?.nodes.map((copiedNode)=> {
+      const newId = nanoid()
+      const oldId = copiedNode.id
+      if(nodes.some((node)=> node.id === copiedNode.id)){
+        copiedSelection.edges.forEach((copiedEdge, index)=>{
+          if(copiedEdge.source === copiedNode.id){
+            copiedSelection.edges[index].source = newId
+          }
+          if(copiedEdge.target === copiedNode.id){
+            copiedSelection.edges[index].target = newId
+          }
+        })
+        copiedNode.id = newId
+      }
+
+      // checking/changing if there is any node whose parentNode should be updated
+      copiedSelection.nodes.forEach((node, index)=> {
+        if(node.parentNode === oldId){
+          copiedSelection.nodes[index].parentNode = newId
+        }
+      })
+      return copiedNode
+    }) as Node[]
+    
+    // replacing duplicate edge's id by new id
+    (copiedSelection as OnSelectionChangeParams).edges = copiedSelection?.edges.map((copiedEdge)=>{
+      if(edges.some((edge)=> edge.id === copiedEdge.id)){
+        const newId = nanoid()
+        copiedEdge.id = newId
+      }
+      return copiedEdge
+    }) as Edge[]
+
+    // we can also create a action in the state that does the same thing as bellow 
+    // but we are using the builtin functionality
+    reactFlowInstance.setNodes(nodes.concat(copiedSelection?.nodes as Node[]))
+    reactFlowInstance.setEdges(edges.concat(copiedSelection?.edges as Edge[]))
+  }, [copiedSelection, edges, nodes, reactFlowInstance])
+
   return (
     <div className='flowWrapper'>
       {showModal && <Modal></Modal>}
@@ -110,14 +172,20 @@ function App() {
         onConnectStart={onConnectStart}
         onConnectEnd={onConnectEnd}
         onNodeClick={onNodeClick}
+        onSelectionChange={onSelectionChange}
         fitView>
-        <Panel position='top-left' color='black'> React Flow Demo </Panel>
-        <Panel position='top-right'> press 'Del' to delete a selected element</Panel>
+        <Panel position='top-left'> React Flow Demo </Panel>
+        <Panel position='top-right' className='copy-paste-panel'>
+          <button className='copy-paste-btn' onClick={cutHandler} disabled={!canCopy}>cut</button>
+          <button className='copy-paste-btn' onClick={copyHandler} disabled={!canCopy}>copy</button>
+          <button className='copy-paste-btn' onClick={pasteHandler} >paste</button>
+        </Panel>
+        <Panel position='bottom-center'> press 'Del' to delete a selected element</Panel>
         <Controls showInteractive={false}></Controls>
-        <MiniMap nodeColor={nodeColor} nodeStrokeWidth={3} zoomable pannable   ></MiniMap>
+        <MiniMap nodeColor={nodeColor} nodeStrokeWidth={3} zoomable pannable></MiniMap>
       </ReactFlow>
     </div>
   )
-}
+})
 
 export default App
